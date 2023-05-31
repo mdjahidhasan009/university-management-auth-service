4a55abb2309b39f4f60805f206d1c9c97ce82f05
mdjahidhasan009 <jahid60012@gmail.com>
2023-06-01 01:53:23 +0600

    Mission_3-Module_11-Video_11-5 install eslint, prettier

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-import express, {Application} from 'express';
+import express, {Application, Request, Response} from 'express';
+import cors from 'cors';
 
 const app : Application = express();
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Working Successfully')
+})
 
 export default app;

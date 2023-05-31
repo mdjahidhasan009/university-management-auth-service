aa6969784805a763cb9d5ab18e2ffe9d1c68336e
mdjahidhasan009 <jahid60012@gmail.com>
2023-06-01 02:20:53 +0600

    Mission_3-Module_11-Video_11-6 install and configure husky

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, {Application, Request, Response} from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app : Application = express();
-app.use(cors());
+const app: Application = express()
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
-export default app;
+export default app

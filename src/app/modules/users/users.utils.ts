0bd56895ab74e3d78386986db44ef8dd78118469
mdjahidhasan009 <jahid60012@gmail.com>
2023-06-02 01:17:08 +0600

    Mission_3-Module_11_Video_11-11 config lint-staged, created interface, model, service, utils, route, controller for user

---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,17 @@
+import { User } from './users.model'
+
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+
+  console.log('lastUser', lastUser)
+
+  return lastUser?.id
+}
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ console.log(path.join(process.cwd(), '.env'))
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
 }

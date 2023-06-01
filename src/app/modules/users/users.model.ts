94425125362074592ddec0ff00fd1d62bc5df074
mdjahidhasan009 <jahid60012@gmail.com>
2023-06-01 06:21:22 +0600

    Mission_3-Module_11-Video_11-8 create_interface_and_model

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,27 @@
+import { IUser } from './users.interface'
+import { model, Model, Schema } from 'mongoose'
+
+type UserModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const User = model<IUser, UserModel>('User', userSchema)

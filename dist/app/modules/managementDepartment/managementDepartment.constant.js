1573b4e86b9ea24294afff9613dc7864fa32eeae
mdjahidhasan009 <jahid60012@gmail.com>
2023-11-21 09:46:21 +0600

    updated gitignore

--- dist/app/modules/managementDepartment/managementDepartment.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.managementDepartmentSearchableFields = exports.managementDepartmentFilterableFields = void 0;
-exports.managementDepartmentFilterableFields = ['searchTerm', 'title'];
-exports.managementDepartmentSearchableFields = ['title'];
--- dist/app/modules/user/user.interface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// type UserModel = Model<IUser, object>
-// export type UserModel = Model<IUser, Record<string, unknown>, IUserMethods>;//For using instance methods
